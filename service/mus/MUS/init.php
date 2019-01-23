ba2371cd14039fc88c11f58113b7e958013589a5
Patrick van Efferen <patrickvanefferen@gmail.com>
2019-01-23 16:20:30 +0100

    Replaced default passwords and simplified config handling

--- service/mus/MUS/init.php
+++ service/mus/MUS/init.php
@@ -1,34 +1,28 @@
 <?php
-/*
+
+/**
  * Use a simple function for settings
+ *
+ * @throws \Exception thrown when configuration could not be set.
  */
 function initializeMus() {
-  $slim = Slim::getInstance();
-  // Get the mode (legacy or normal)
-  $conf_filename = 'conf.php';
-  $legacy_setting = $slim->request()->params('legacymode');
-  if (isset($legacy_setting) && $legacy_setting == 'on' && file_exists(MUS_ROOT . '/environment/legacy_conf.php')) {
-    $conf_filename = 'legacy_conf.php';
-  }
-  $settings = array();
-  // try to get the conf from the environment
-  if (file_exists(MUS_ROOT . '/environment/' . $conf_filename)) {
-    include_once MUS_ROOT . '/environment/' . $conf_filename;
-  }
-  if (isset($conf)) {
-    $settings = $conf;
-  }
-  else {
-    $settings['debug'] = TRUE;
-    $settings['mode'] = 'development';
-    $settings['db_database'] = 'gemeenapi';
-    $settings['db_username'] = 'gemeenapi';
-    $settings['db_password'] = 'z31lb00t';
-    $settings['db_host'] = 'localhost';
-    $settings['solr_host'] = '10.211.55.25';
-    $settings['solr_port'] = '8080';
-    $settings['solr_path'] = '/solr36/gemeentemuseum';
-  }
-  // Now add the settings to Slim
-  $slim->config($settings);
+    $slim = Slim::getInstance();
+    // Get the mode (legacy or normal)
+    $conf_filename = 'conf.php';
+    $legacy_setting = $slim->request()->params('legacymode');
+    if (isset($legacy_setting) && $legacy_setting == 'on' && file_exists(MUS_ROOT . '/environment/legacy_conf.php')) {
+        $conf_filename = 'legacy_conf.php';
+    }
+
+    // try to get the conf from the environment
+    if (file_exists(MUS_ROOT . '/environment/' . $conf_filename)) {
+        include_once MUS_ROOT . '/environment/' . $conf_filename;
+    }
+    if (isset($conf)) {
+        // Now add the settings to Slim
+        $slim->config($conf);
+    }
+    else {
+        throw new Exception('Configuration not found');
+    }
 }
