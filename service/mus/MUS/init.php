1d076b66d6b89f3ff87fc567510464da4907aa1c
Patrick van Efferen <patrickvanefferen@gmail.com>
2022-11-25 11:56:04 +0100

    Added code from Mus production

--- service/mus/MUS/init.php
+++ service/mus/MUS/init.php
@@ -1,28 +1,27 @@
 <?php
-
 /**
  * Use a simple function for settings
  *
  * @throws \Exception thrown when configuration could not be set.
  */
 function initializeMus() {
-    $slim = Slim::getInstance();
-    // Get the mode (legacy or normal)
-    $conf_filename = 'conf.php';
-    $legacy_setting = $slim->request()->params('legacymode');
-    if (isset($legacy_setting) && $legacy_setting == 'on' && file_exists(MUS_ROOT . '/environment/legacy_conf.php')) {
-        $conf_filename = 'legacy_conf.php';
-    }
+  $slim = Slim::getInstance();
+  // Get the mode (legacy or normal)
+  $conf_filename = 'conf.php';
+  $legacy_setting = $slim->request()->params('legacymode');
+  if (isset($legacy_setting) && $legacy_setting == 'on' && file_exists(MUS_ROOT . '/environment/legacy_conf.php')) {
+    $conf_filename = 'legacy_conf.php';
+  }
 
-    // try to get the conf from the environment
-    if (file_exists(MUS_ROOT . '/environment/' . $conf_filename)) {
-        include_once MUS_ROOT . '/environment/' . $conf_filename;
-    }
-    if (isset($conf)) {
-        // Now add the settings to Slim
-        $slim->config($conf);
-    }
-    else {
-        throw new Exception('Configuration not found');
-    }
+  $settings = array();
+  // try to get the conf from the environment
+  if (file_exists(MUS_ROOT . '/environment/' . $conf_filename)) {
+    include_once MUS_ROOT . '/environment/' . $conf_filename;
+  }
+  if (isset($conf)) {
+    $slim->config($conf);
+  }
+  else {
+    throw new Exception('Configuration not found');
+  }
 }
